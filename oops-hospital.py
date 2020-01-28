f97e100be7fe9bc3738cc04f4afdbc1ee4ee9920
E.Karthik <kartheekenumarthi@gmail.com>
2020-01-28 17:26:22 +0530

    on 28 feb

---
+++ oops-hospital.py
@@ -0,0 +1,17 @@
+class hospital:
+    fee = 500
+    def __init__(self,name,time):
+        self.name = name
+        self.time = time
+    def checkup(self):
+        print("pay fee of",self.fee,"for",self.name,"at",self.time)
+    @classmethod
+    def feeinc(cls):
+        cls.fee = cls.fee + 500
+    
+hosp1 = hospital("apollo" , "12-30")
+hosp1.checkup()
+hospital.feeinc()
+hosp2 = hospital("kims","1-00")
+hosp2.checkup()
+        
