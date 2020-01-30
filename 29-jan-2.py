aeed08e0f6d4e0a4aee17534ad88b7e408181ba2
E.Karthik <kartheekenumarthi@gmail.com>
2020-01-30 22:34:18 +0530

    commit on master

--- 29-jan-2.py
+++ 29-jan-2.py
@@ -1,14 +1,51 @@
 #multiple inheritance 
-class car:
-    def __init__(self,name):
-        print(name)
-class bmw(car):
-    def __init__(self,name,model):
-        super().__init__(name)
-        print("bmw model:",model)
-class maruthi(car):
-    def __init__(self,name,model):
-        super().__init__(name)
-        print("maruthi model:",model)
-c = bmw("car","x20")
-x = maruthi(" car"," u5000")
+# class car:
+#     def __init__(self,name):
+#         print(name)
+# class bmw(car):
+#     def __init__(self,name,model):
+#         super().__init__(name)
+#         print("bmw model:",model)
+# class maruthi(car):
+#     def __init__(self,name,model):
+#         super().__init__(name)
+#         print("maruthi model:",model)
+# c = bmw("car","x20")
+# x = maruthi(" car"," u5000")
+
+#multilevel inheritance 
+
+# class a:
+#     def __init__(self,var_a):
+#         print("from a",var_a)
+#     def fun(self):
+#         print("123")
+# class b(a):
+#     def __init__(self,var_a,var_b):
+#         super().__init__(var_a)
+#         print("from b",var_b)
+# class c(b):
+#     pass
+#     def __init__(self, var_a, var_b, var_c):
+#         super().__init__(var_a, var_b)
+#         print("from c",var_c)
+#         super().fun()
+# c1 = c(1,2,3)
+
+
+#multiple inheritance
+#more than one parent class 
+
+
+class father:
+    def __init__(self):
+        print("from father")
+class mother:
+    def __init__(self):
+        print("from mother")
+class son(father,mother):
+    def __init__(self):
+        super().__init__()
+        print("from son")
+
+son1 = son()
---
+++ 30-jan-3.py
@@ -0,0 +1,50 @@
+# import keyword
+# print(keyword.iskeyword("for")) - True
+
+
+# print("hello","world",sep="-",end=" ")  
+# print("hello","world",sep="-",end=" ")
+#o/p - hello-world hello-world
+
+
+# str = "I love geeksforgeeks"
+# # Printing the center aligned   
+# # string with fillchr  
+# print ("Center aligned string with fillchr: ")  
+# print (str.center(40, '#'))  
+# # Printing the left aligned   
+# # string with "-" padding   
+# print ("The left aligned string is : ")  
+# print (str.ljust(40, '-'))  
+# # Printing the right aligned string  
+# # with "-" padding   
+# print ("The right aligned string is : ")  
+# # print (str.rjust(40, '-')) 
+# l = "string".center(40, '-')
+# print(l)
+
+
+#string formatting by different methods using % , {} , template strings
+# #% formatting is similar to printf in c
+# # %d - int , %f - float , %s - string , %x - hexadecimal , %o - octal , %r - raw data
+# var = '15'
+# print("%s"%(var)) #- 15
+# # print("%d"%(var)) #- error num is req
+# # print("%f"%(var)) #- error num is req
+# print("%r"%(var)) #- '15'
+# var = 15
+# print("%x"%(var)) #- '15'
+# print("%o"%(var)) #- '15'
+# print("%d"%(var)) # - 15
+# print("%f"%(var)) # - 15.000000
+
+
+
+
+
+
+
+
+
+
+
