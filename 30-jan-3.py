1fb903bda9e1dc3144f47e5334039fbfda1e85f3
E.Karthik <kartheekenumarthi@gmail.com>
2020-01-30 23:08:17 +0530

    basics added

--- 30-jan-3.py
+++ 30-jan-3.py
@@ -25,6 +25,7 @@
 
 
 #string formatting by different methods using % , {} , template strings
+
 # #% formatting is similar to printf in c
 # # %d - int , %f - float , %s - string , %x - hexadecimal , %o - octal , %r - raw data
 # var = '15'
@@ -37,12 +38,32 @@
 # print("%o"%(var)) #- '15'
 # print("%d"%(var)) # - 15
 # print("%f"%(var)) # - 15.000000
+# # %[flags][width][.precision]type 
+# print("hello:%2.2d world:%1d"%(123.45,123.456))
+# # Python program showing how to use 
+# # string modulo operator(%) to print 
+# # fancier output 
+# # print integer and float value 
+# print("Geeks : % 2d, Portal : % 5.2f" %(1, 05.333))  
+# # print integer value 
+# print("Total students : % 3d, Boys : % 2d" %(240, 120)) 
+# # print octal value 
+# print("% 7.3o"% (25))   
+# # print exponential value 
+# print("% 10.3E"% (356.08977)) 
+
+# print("{} for {}".format(1,2))
+# print("{1} for {0}".format(1,2))
+# print("{0},{1},{some}".format(4,5,some = 3))
 
 
-
-
-
-
+#---------------------------------------------------------------------------------------
+#-----------------------------------strings-------------------------------------------------------
+# str = "hello"
+# str2 = '''
+# world
+# hi'''
+# print(str2)
 
 
 
