a77bcd8b35dd5e0e26661b2763d5fe9762d32c95
E.Karthik <kartheekenumarthi@gmail.com>
2020-01-30 17:07:09 +0530

    added multiple inheritance

--- 29-jan-2.py
+++ 29-jan-2.py
@@ -1,5 +1,14 @@
+#multiple inheritance 
 class car:
     def __init__(self,name):
-        print(self.name)
-class details(car):
-    def __init__()
+        print(name)
+class bmw(car):
+    def __init__(self,name,model):
+        super().__init__(name)
+        print("bmw model:",model)
+class maruthi(car):
+    def __init__(self,name,model):
+        super().__init__(name)
+        print("maruthi model:",model)
+c = bmw("car","x20")
+x = maruthi(" car"," u5000")
