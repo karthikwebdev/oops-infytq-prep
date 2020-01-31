53c5bf540fe1730c27995b558fcd93a3c68a2913
E.Karthik <kartheekenumarthi@gmail.com>
2020-01-31 16:59:02 +0530

    strted 31 jan

---
+++ 31-jan.py
@@ -0,0 +1,19 @@
+#multiple inheritance
+class father:
+    def __init__(self):
+        self.acres = 10
+    def getf(self):
+        return father().acres
+
+class mother:
+    def __init__(self):
+        self.acres = 5
+    def getm(self):
+        return mother().acres
+
+class son(mother,father):
+    def __init__(self):
+        self.acres = self.getm() + self.getf()
+        print(self.acres)
+
+s = son()
